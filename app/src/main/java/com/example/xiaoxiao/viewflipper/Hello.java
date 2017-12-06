1d3e2c12b7c9a8a44c3d3798538c6470444ef056
xiao-er <1947478978@qq.com>
2017-12-06 11:44:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiaoxiao/viewflipper/Hello.java
@@ -0,0 +1,11 @@
+package com.example.xiaoxiao.viewflipper;
+
+/**
+ * file name文件名：Hello
+ * Description 描述：
+ * Created time 创建时间：2017/12/6 11:03
+ * author 作者：潇潇
+ * email：15010222898@163.com
+ */
+public class Hello {
+}
