7ce6fb9e18563d315beda9f14982289eb0dfa788
xiao-er <1947478978@qq.com>
2017-12-08 09:58:31 +0800

    添加string

--- app/src/main/java/com/example/xiaoxiao/viewflipper/MainActivity.java
+++ app/src/main/java/com/example/xiaoxiao/viewflipper/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.xiaoxiao.viewflipper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -18,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtContent;
 
 
-
     //数据源
     private List<String> dataList = Arrays.asList(
             "没有你的世界，就像没有暑假的八月 \n" +
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             "我还可以默默地喜欢你多久、你还可以在我心里呆多久、、、是你自己走出去、还是我放了你、、、",
             "好好珍惜那个人带给你的伤痛，好好享受爱情留下的痕迹。多年以后，当激情归于平淡，当生活变得麻木，这种伤痛就成了得不到的奢侈品。回头想起那个人的时候，我会说，谢谢你，带给我伤痛。痛，是因为爱过!",
             "幸福不是获得多了，而是在乎少了;活得糊涂的人，容易幸福;活得清醒的人，容易烦恼。清醒的人看得太真切，一较真，生活中便烦恼遍地;糊涂的人，计较得少，虽然活得简单粗糙，却因此觅得了人生的大滋味。"
-            );//数据源
+    );//数据源
 
 
     @Override
@@ -40,14 +40,14 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         viewFlipper = (ViewFlipper) findViewById(R.id.vf);
         for (int i = 0; i < dataList.size(); i++) {
-            View view = LayoutInflater.from(this).inflate(R.layout.childviewflipper,null);
+            View view = LayoutInflater.from(this).inflate(R.layout.childviewflipper, null);
             txtContent = view.findViewById(R.id.txt_content);
             txtContent.setText(dataList.get(i).toString());
             final int finalI = i;
             txtContent.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this,dataList.get(finalI).toString(),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, dataList.get(finalI).toString(), Toast.LENGTH_SHORT).show();
                 }
             });
             viewFlipper.addView(view);
